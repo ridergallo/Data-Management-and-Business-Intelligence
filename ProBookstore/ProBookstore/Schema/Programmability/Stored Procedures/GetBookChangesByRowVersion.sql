588f8bcb79f4aa85cd038672c6f34a987be46cfa
ridergallo <riderarvin.gallo@gmail.com>
2025-11-30 17:10:03 -0400

    Modificaciones

--- ProBookstore/ProBookstore/Schema/Programmability/Stored Procedures/GetBookChangesByRowVersion.sql
+++ ProBookstore/ProBookstore/Schema/Programmability/Stored Procedures/GetBookChangesByRowVersion.sql
@@ -6,20 +6,21 @@
 AS
 BEGIN
 
-  SELECT b.[book_id]
-      ,b.[title]
-      ,b.[isbn13]
-      ,a.[author_name] 
-      ,bl.[language_code]
-      ,bl.[language_name]
-      ,p.[publisher_name]
-      ,b.[publication_date]
-      ,b.[num_pages]
-    FROM [dbo].[book] b
+    SELECT b.[book_id]
+          ,b.[title]
+          ,b.[isbn13]
+          ,a.[author_name] 
+          ,bl.[language_code]
+          ,bl.[language_name]
+          ,p.[publisher_name]
+          ,b.[publication_date]
+          ,b.[num_pages]
+        FROM [dbo].[book] b
     INNER JOIN [dbo].[book_language] bl ON b.language_id = bl.language_id
     INNER JOIN [dbo].[publisher] p ON b.publisher_id = p.publisher_id
-    INNER JOIN [dbo].[book_author] ba ON b.book_id = ba.book_id
-    INNER JOIN [dbo].[author] a ON ba.author_id = a.author_id
+    LEFT JOIN (select book_id, max(author_id) as author_id, max(rowversion) as rowversion 
+                  from [dbo].[book_author] group by book_id) ba ON b.book_id = ba.book_id
+    LEFT JOIN [dbo].[author] a ON ba.author_id = a.author_id
     WHERE (b.[rowversion] > CONVERT(ROWVERSION,@startRow) AND b.[rowversion] <= CONVERT(ROWVERSION,@endRow))
   OR (bl.[rowversion] > CONVERT(ROWVERSION,@startRow) AND bl.[rowversion] <= CONVERT(ROWVERSION,@endRow))
   OR (p.[rowversion] > CONVERT(ROWVERSION,@startRow) AND p.[rowversion] <= CONVERT(ROWVERSION,@endRow))
--- ProBookstore/ProBookstore/Schema/Programmability/Stored Procedures/GetCustomerChangesByRowVersion.sql
+++ ProBookstore/ProBookstore/Schema/Programmability/Stored Procedures/GetCustomerChangesByRowVersion.sql
@@ -15,11 +15,14 @@ BEGIN
 		  ,a.city
 		  ,co.country_name
 		  ,adds.address_status
-  FROM [dbo].[customer] c
-  JOIN [dbo].[customer_address] ca ON (c.customer_id = ca.customer_id)
-  JOIN [dbo].[address_status] adds ON (ca.status_id = adds.status_id)
-  JOIN [dbo].[address] a ON (ca.address_id = a.address_id)
-  JOIN [dbo].[country] co on (a.country_id = co.country_id)
+	  FROM [dbo].[customer] c
+	LEFT JOIN (select customer_id, max(address_id) as address_id
+			  from [dbo].[customer_address] 
+			group by customer_id) cam ON (c.customer_id = cam.customer_id)
+	LEFT JOIN [dbo].[customer_address] ca ON (ca.customer_id = cam.customer_id and ca.address_id = cam.address_id)
+	LEFT JOIN [dbo].[address_status] adds ON (ca.status_id = adds.status_id)
+	LEFT JOIN [dbo].[address] a ON (ca.address_id = a.address_id)
+	LEFT JOIN [dbo].[country] co on (a.country_id = co.country_id)
   WHERE (c.[rowversion] > CONVERT(ROWVERSION,@startRow) AND c.[rowversion] <= CONVERT(ROWVERSION,@endRow))
   OR (ca.[rowversion] > CONVERT(ROWVERSION,@startRow) AND ca.[rowversion] <= CONVERT(ROWVERSION,@endRow))
   OR (adds.[rowversion] > CONVERT(ROWVERSION,@startRow) AND adds.[rowversion] <= CONVERT(ROWVERSION,@endRow))
